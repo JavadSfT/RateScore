d2252522575f52b365c0a9bb4113bb1676eedee6
Javad <Javad.Sf.Talebi@outlook.com>
2023-02-01 17:36:08 +0330

    Ajax and fetching

--- Script/Metacritic.js
+++ Script/Metacritic.js
@@ -1,7 +1,49 @@
-// create data class
+class MetacriticData {
+    Picture;
+    Rate;
+    Name;
+    Date;
+    Description;
+}
+let MetaBox = [];
 
-// create ajax data
+const MetaData = new XMLHttpRequest();
 
-// Write logic
+MetaData.onreadystatechange = () => {
+    if (MetaData.readyState === XMLHttpRequest.DONE) {
 
-// send ajax data
+        if (MetaData.status === 200) {
+
+            MetaData.onload = () => {
+                let data = MetaData.responseText;
+
+                let elm = document.getElementById("");
+                elm.innerHTML = data;
+                elm.style.display = "none";
+
+                for (let i = 0; i < 10; i++) {
+                    let subData = new MetacriticData();
+                    // subData.Picture = document.querySelectorAll(".result_wrap img")[i].childNodes[0].nodeValue;
+                    subData.Rate = document.querySelectorAll(".metascore_w")[i].childNodes[0].nodeValue;
+                    subData.Name = document.querySelectorAll(".product_title a")[i].childNodes[0].nodeValue;
+                    subData.Date = document.querySelectorAll(".main_stats p")[i].childNodes[0].nodeValue;
+                    subData.Description = document.querySelectorAll(".deck")[i].childNodes[0].nodeValue;
+                    MetaBox.push(subData);
+                }
+
+                console.log(MetaBox);
+
+                let showText = "";
+                for(let i = 0 ; i<MetaBox.length;i++){
+                    showText += `${MetaBox[i].Rate} <br> ${MetaBox[i].Name} <br>
+                     ${MetaBox[i].Date} <br> ${MetaBox[i].Description} <hr>`;
+                }
+            }
+        }
+    }
+}
+
+MetaData.open("Get", "https://www.metacritic.com/search/all/avatar/results", true);
+
+MetaData.setRequestHeader();
+MetaData.send();
